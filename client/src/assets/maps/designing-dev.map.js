c14d470bfe0901209974c659ff91dbabc609087e
nandhakumar18 <sangainandhu18@gmail.com>
2022-09-22 18:25:32 +0530

    before sambavom

---
+++ client/src/assets/maps/designing-dev.map.js
@@ -0,0 +1,20 @@
+export default {
+  tittle: "designing-roadmap",
+    thumbnail: "web_dev_roadmap.gif",
+code:"designing_roadmap",
+  chapters: [{
+    tittle: "intro",
+    lessons: [{
+      type: "video"
+    }, {
+      type: "link"
+    }]
+  }]
+}
+
+
+
+// --vp-c-text-dark-1: rgba(255, 255, 255, .87);
+// --vp - c - text - dark - 2: rgba(235, 235, 235, .6);
+// --vp - c - text - dark - 3: rgba(235, 235, 235, .38);
+// --vp - c - text - dark - 4: rgba(235, 235, 235, .18);
